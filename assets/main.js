9e33a8b9764fe011135185556b24a815e298c425
Mohin Uddin <csemohin@gmail.com>
2019-01-29 21:58:36 +0600

    update v.1.0.1

--- assets/main.js
+++ assets/main.js
@@ -1,9 +1,7 @@
 var search = document.querySelector("#search");
-var icon = document.querySelector("#icon");
 var inp = document.querySelector(".inp");
 search.addEventListener("click",()=>{
     search.style.width = "20%";
-    search.style.paddingLeft = "50px";
     search.style.cursor = "text";
     search.style.cursor = "text";
     search.setAttribute("placeholder", "Type Anything");
